e50e126da3d510d9c4e8e638cdc026bc6d9a5925
Sabino Capingana <scapingana@gmail.com>
2024-10-13 22:30:09 +0100

    actualizado a data

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,10 @@
 const hamburguer = document.querySelector(".nav-humberguer");
+const data= document.querySelector("#data");
 const navmenu = document.querySelector(".nav-menu");
 hamburguer.addEventListener("click", () => {
   hamburguer.classList.toggle("active");
   navmenu.classList.toggle("active");
 });
+
+let novadata=new Date();
+data.innerHTML=novadata.getFullYear();
